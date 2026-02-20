ed181b4e446ee5f251ae798eec5a442a049c490d
Zhanna Petrova <zhanna.petrova@gmail.com>
2026-02-20 17:53:49 +0300

    Fix API URL configuration and add SPA frontend

--- table/urls.py
+++ table/urls.py
@@ -6,5 +6,5 @@ router = DefaultRouter()
 router.register(r'items', views.TableItemViewSet, basename='tableitem')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),  # Убрали 'api/' отсюда!
 ]
